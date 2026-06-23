b1b3536e154f19b07fe44c5a84b064b9e740b9aa
rgdevs2022 <princekushwaha@reddingtonglobal.com>
2026-06-23 18:27:33 +0530

    gtm fix

--- frontend/assets/site.js
+++ frontend/assets/site.js
@@ -7,24 +7,6 @@
 (function () {
   "use strict";
 
-  /* ---- Google Tag Manager ---- */
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W26G2389');
-
-  /* ---- Google Analytics (gtag.js) ---- */
-  (function(){var s=document.createElement('script');s.async=true;s.src='https://www.googletagmanager.com/gtag/js?id=G-VVJKM8TE6S';document.head.appendChild(s);})();
-  window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-VVJKM8TE6S');
-
-  /* ---- GTM noscript fallback (injected as first child of body) ---- */
-  document.addEventListener('DOMContentLoaded', function() {
-    var ns = document.createElement('noscript');
-    var iframe = document.createElement('iframe');
-    iframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-W26G2389';
-    iframe.height = '0'; iframe.width = '0';
-    iframe.style.cssText = 'display:none;visibility:hidden';
-    ns.appendChild(iframe);
-    document.body.insertBefore(ns, document.body.firstChild);
-  });
-
   window.CASHBRIDGE = window.CASHBRIDGE || {};
   var CFG = Object.assign({
     brand: "MyCashBridge",
